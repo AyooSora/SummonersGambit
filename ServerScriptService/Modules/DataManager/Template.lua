9663b21a8fda071d7a5498884d5b0a611a011df8
AyooSora <ayoosoraa2202@gmail.com>
2026-01-01 05:29:16 -0500

    Updated ServerScriptService/Modules/DataManager/Template.lua

--- ServerScriptService/Modules/DataManager/Template.lua
+++ ServerScriptService/Modules/DataManager/Template.lua
@@ -1,13 +1,12 @@
 -- @ScriptType: ModuleScript
 local module = {
-	Yen = 2000,
+	Yen = 20000,
 	Gems = 5000,
 	Level = 1,
 	PullsSinceMythic = 0,
 	PullsSinceDivine = 0,
 	MinCards = 40,
 	MaxCards = 60,
-	
 	
 	Cards = {
 		["AUnit"] = 0,
@@ -16,7 +15,16 @@ local module = {
 		["DUnit"] = 0,
 		["EUnit"] = 0,
 		["FUnit"] = 0,
-	}
+	},
+	
+	CardPotential = {
+		["AUnit"] = 0,
+		["BUnit"] = 0,
+		["CUnit"] = 0,
+		["DUnit"] = 0,
+		["EUnit"] = 0,
+		["FUnit"] = 0,
+	},
 }
 
 
