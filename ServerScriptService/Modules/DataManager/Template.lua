2baa5686e7868b5df74eb7146c501b9ae3378fbc
AyooSora <ayoosoraa2202@gmail.com>
2025-12-31 22:41:05 -0500

    Updated ServerScriptService/Modules/DataManager/Template.lua

---
+++ ServerScriptService/Modules/DataManager/Template.lua
@@ -0,0 +1,18 @@
+-- @ScriptType: ModuleScript
+local module = {
+	Yen = 2000,
+	Gems = 5000,
+	Level = 1,
+	PullsSinceMythic = 0,
+	PullsSinceDivine = 0,
+	MinCards = 40,
+	MaxCards = 60,
+	
+	
+	Cards = {
+		["AUnit"] = 0,
+	}
+}
+
+
+return module
