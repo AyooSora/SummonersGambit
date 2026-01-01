7a08eb3ef76fed0b5bfdd145dd111e0c01df6cef
AyooSora <ayoosoraa2202@gmail.com>
2025-12-31 23:50:49 -0500

    Updated ReplicatedStorage/Modules/CardUI.lua

---
+++ ReplicatedStorage/Modules/CardUI.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Chance = require(ReplicatedStorage:WaitForChild("Modules"):WaitForChild("Chance"))
+
+local CardUI = {}
+
+function CardUI.CreateCardFrame(cardName : string, player : Player, parent)
+	
+end
+
+
+return CardUI
